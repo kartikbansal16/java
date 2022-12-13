212d3ea60da9687f16bd023dc254a1b4f79f7b2c
kartikbansal16 <104322752+kartikbansal16@users.noreply.github.com>
2022-12-13 15:22:34 +0530

    Add files via upload

---
+++ whitespaceRemove.java
@@ -0,0 +1,7 @@
+public class whitespaceRemove {
+    public static void main(String[] args){
+        String s1 = "MY NAME IS KARTIK";
+        String s2 = s1.replaceAll(" ", "");
+        System.out.println(s2);
+    }
+}
