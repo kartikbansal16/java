9f0a082f604e48681be027d907469be773146850
kartikbansal16 <104322752+kartikbansal16@users.noreply.github.com>
2023-01-10 14:34:52 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,60 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Login() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");  
+	    PrintWriter out = response.getWriter();  
+	          
+	    String n=request.getParameter("userName");  
+	    String p=request.getParameter("userPass");  
+	          
+	    if(n.equals("servlet")&& p.equals("12345")){ 
+	        RequestDispatcher rd=request.getRequestDispatcher("Success");  
+	        rd.forward(request, response);  
+	    }  
+	    else{  
+	    	
+	    	out.print("<center>Sorry UserName or Password Error</center>");  
+	        RequestDispatcher rd=request.getRequestDispatcher("Login.html");  
+	        rd.include(request, response);  
+	                      
+	        }  
+	    }  
+	
+		
+
+}
